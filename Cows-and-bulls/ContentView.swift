4437072c30f508834bac2812d54f5797f40f904a
Aarni Klemetti <aarni.klemetti@gmail.com>
2022-07-08 16:33:39 +0300

    Initial Commit

---
+++ Cows-and-bulls/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cows-and-bulls
+//
+//  Created by Aarni on 8.7.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cows-and-bulls/Cows_and_bullsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cows_and_bullsApp.swift
+//  Cows-and-bulls
+//
+//  Created by Aarni on 8.7.2022.
+//
+
+import SwiftUI
+
+@main
+struct Cows_and_bullsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
